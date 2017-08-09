3ce8f492ca314b5f07e39267629c8be89a86cb26
rosch <yoansalas76@gmail.com1>
2017-08-09 03:04:36 -0600

    Creando las clases cliente y tipoEmpleado en businerss

---
+++ PhpProjectVenta/businerss/tipoEmpleadoAccion.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Descripcion valida que los datos y la accion a realizar sean correcta sino redirecciona
+ * a paguinas de error y si los datos y la accion es correcta imboca a la businerss para
+ * que se haga cargo de los datos y el metodo a efectuar.
+ *
+ * @author David Salas.
+ */
+class tipoEmpleadoAccion {
+    //put your code here
+}
---
+++ PhpProjectVenta/businerss/tipoEmpleadoBusiness.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Descripcion es el encargado de agarrar los datos mandados y efectuar el metodo
+ * correspondiente, inbocando a el metodo de data persona y pasandole como parameros los datos
+ * recogidos en las variables.
+ *
+ * @author David Salas Lorente
+ */
+class tipoEmpleadoBusiness {
+    //put your code here
+}
