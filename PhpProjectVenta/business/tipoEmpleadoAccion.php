91f85aefef165e6aeef8c4d4a696bab22b29d7ae
rosch <yoansalas76@gmail.com1>
2017-08-20 17:33:47 -0600

    modificado el tipoEmpleadoAccion de businerss

--- PhpProjectVenta/business/tipoEmpleadoAccion.php
+++ PhpProjectVenta/business/tipoEmpleadoAccion.php
@@ -8,37 +8,27 @@
  */
 include './tipoEmpleadoBusiness.php';
 
-if (isset($_POST['update'])) {
+if (isset($_POST['update'])){
 
-    if (isset($_POST['IdTipoEmpleado']) && isset($_POST['CedulaTipoEmpleado']) && isset($_POST['NombreTipoEmpleado']) && isset($_POST['Apellido1TipoEmpleado']) && isset($_POST['Apellido2TipoEmpleado'])
-            && isset($_POST['EdadTipoEmpleado']) && isset($_POST['SexoTipoEmpleado']) && isset($_POST['EstadoTipoEmpleado'])
-             && isset($_POST['Telefono1TipoEmpleado']) && isset($_POST['Telefono2TipoEmpleado']) && isset($_POST['CorreoTipoEmpleado']) && isset($_POST['DireccionTipoEmpleado']) && isset($_POST['CuentaBancariaTipoEmpleado'])) {
-            
-        $idTipoempleado = $_POST['IdTipoEmpleado'];
-        $cedulaTipoempleado = $_POST['CedulaTipoEmpleado'];
-        $nombreTipoEmpleado = $_POST['NombreTipoEmpleado'];
-        $apellido1TipoEmpleado= $_POST['Apellido1TipoEmpleado'];
-        $apellido2TipoEmpleado= $_POST['Apellido2TipoEmpleado'];
-        $edadTipoEmpleado= $_POST['EdadTipoEmpleado'];
-        $sexoTipoEmpleado= $_POST['SexoTipoEmpleado'];
-        $estadoTipoEmpleado= $_POST['EstadoTipoEmpleado'];
-        $telefono1TipoEmpleado= $_POST['Telefono1TipoEmpleado'];
-        $telefono2TipoEmpleado= $_POST['Telefono2TipoEmpleado'];
-        $correoTipoEmpleado= $_POST['CorreoTipoEmpleado'];
-        $direccionTipoEmpleado= $_POST['DireccionTipoEmpleado'];
-        $cuentaBancariaTipoEmpleado= $_POST['CuentaBancariaTipoEmpleado'];
-
-        if (strlen($idTipoempleado) > 0 && strlen($cedulaTipoempleado) > 0 && strlen($nombreTipoEmpleado) > 0 && strlen($apellido1TipoEmpleado) > 0 
-                && strlen($apellido2TipoEmpleado) > 0 && strlen($edadTipoEmpleado) > 0 && strlen($telefono1TipoEmpleado) > 0 
-                && strlen($telefono2TipoEmpleado) > 0 && strlen($correoTipoEmpleado) > 0 && strlen($direccionTipoEmpleado) > 0
-                && strlen($cuentaBancariaTipoEmpleado) > 0) {
+    if (isset($_POST['idEmpleado']) && isset($_POST['nombrePersona']) && isset($_POST['apellido1Persona']) && isset($_POST['apellido2Persona'])
+            && isset($_POST['tipoUsuarioPersona']) && isset($_POST['idZona']) && isset($_POST['telefonoPersona']) ) {
+          
+        $telefonoPersona = $_POST('telefonoPersona');
+        $nombrePersona =$_POST('nombrePersona');
+        $apellido1Persona =$_POST('apellido1Persona');
+        $apellido2Persona = $_POST('apellido2Persona');
+        $tipoUsuarioPersona = $_POST('tipoUsuarioPersona');
+        $idZona = $_POST('idZona');
+        $IdEmpleado=$_POST('idEmpleado');
+        
+        if (strlen($IdEmpleado) > 0 &&strlen($nombrePersona) > 0 && strlen($apellido1Persona) > 0 && strlen($apellido2Persona) > 0 && 
+            strlen($tipoUsuarioPersona) > 0 && strlen($idZona) > 0 && strlen($telefonoPersona) > 0 ) {
             if (!is_numeric($nombreTipoEmpleado)) {
-                $empleado = new empleado($idTipoempleado, $cedulaTipoempleado, $nombreTipoEmpleado, $apellido1TipoEmpleado, $apellido2TipoEmpleado, $edadTipoEmpleado,
-                        $telefono1TipoEmpleado, $telefono2TipoEmpleado,$correoTipoEmpleado,$direccionTipoEmpleado,$cuentaBancariaTipoEmpleado);
+                $Persona = new Persona($telefonoPersona, $nombrePersona, $apellido1Persona, $apellido2Persona, $tipoUsuarioPersona, $idZona,IdEmpleado);
 
                 $tipoEmpledoBusiness = new tipoEmpleadoBusiness();
 
-                $result = $tipoEmpleadoBusiness->updateTBTipoEmpleado($tipoEmpleado);
+                $result = $tipoEmpleadoBusiness->updateTBTipoEmpleado($Persona);
                 if ($result == 1) {
                     header("location: ../view/tipoEmpleadoView.php?success=updated");
                 } else {
@@ -73,32 +63,21 @@ if (isset($_POST['update'])) {
     }
 } else if (isset($_POST['Create'])) {
 
-    if (isset($_POST['CedulaTipoEmpleado']) && isset($_POST['NombreTipoEmpleado']) && isset($_POST['Apellido1TipoEmpleado']) && isset($_POST['Apellido2TipoEmpleado'])
-            && isset($_POST['EdadTipoEmpleado']) && isset($_POST['SexoTipoEmpleado']) && isset($_POST['EstadoTipoEmpleado']) && isset($_POST['Telefono1TipoEmpleado'])
-             && isset($_POST['Telefono2TipoEmpleado']) && isset($_POST['CorreoTipoEmpleado']) && isset($_POST['DireccionTipoEmpleado']) && isset($_POST['CuentaBancariaTipoEmpleado'])) {
-            
-        $cedulaTipoempleado=$_POST['CedulaTipoEmpleado'];
-        $nombreTipoempleado=$_POST['NombreTipoEmpleado'];
-        $apellido1Tipoempleado=$_POST['Apellido1TipoEmpleado'];
-        $apellido2Tipoempleado=$_POST['Apellido2TipoEmpleado'];
-        $edadTipoempleado=$_POST['EdadTipoEmpleado'];
-        $sexoTipoempleado=$_POST['SexoTipoEmpleado'];
-        $estadoTipoempleado=$_POST['EstadoTipoEmpleado'];
-        $telefono1Tipoempleado=$_POST['Telefono1TipoEmpleado'];
-        $telefono2Tipoempleado=$_POST['Telefono2TipoEmpleado'];
-        $correoTipoempleado=$_POST['CorreoTipoEmpleado'];
-        $direccionTipoempleado=$_POST['DireccionTipoEmpleado'];
-        $cuentabancariaTipoempleado=$_POST['CuentaBancariaTipoEmpleado'];
+    if (isset($_POST['nombrePersona']) && isset($_POST['apellido1Persona']) && isset($_POST['apellido2Persona'])
+            && isset($_POST['tipoUsuarioPersona']) && isset($_POST['idZona']) && isset($_POST['telefonoPersona']) ) {
+          
+        $telefonoPersona = $_POST('telefonoPersona');
+        $nombrePersona =$_POST('nombrePersona');
+        $apellido1Persona =$_POST('apellido1Persona');
+        $apellido2Persona = $_POST('apellido2Persona');
+        $tipoUsuarioPersona = $_POST('tipoUsuarioPersona');
+        $idZona = $_POST('idZona');
         
 
-        if (strlen($cedulaTipoempleado) > 0 && strlen($nombreTipoEmpleado) > 0 && strlen($apellido1TipoEmpleado) > 0 
-                && strlen($apellido2TipoEmpleado) > 0 && strlen($edadTipoEmpleado) > 0 
-                && strlen($sexoTipoEmpleado) > 0 && strlen($estadoTipoEmpleado) > 0 && strlen($telefono1TipoEmpleado) > 0 && strlen($telefono2TipoEmpleado) > 0
-                 && strlen($correoTipoEmpleado) > 0 && strlen($direccionTipoEmpleado) > 0 && strlen($cuentaBancariaTipoEmpleado) > 0) {
+       if (strlen($nombrePersona) > 0 && strlen($apellido1Persona) > 0 && strlen($apellido2Persona) > 0 && 
+            strlen($tipoUsuarioPersona) > 0 && strlen($idZona) > 0 && strlen($telefonoPersona) > 0 ) {
             if (!is_numeric($nombreTipoEmpleado)) {
-                $tipoEmpleado = new tipoEmpleado(0, $cedulaTipoempleado, $nombreTipoEmpleado, $apellido1TipoEmpleado, $apellido2TipoEmpleado,
-                        $edadTipoEmpleado, $sexoTipoEmpleado, $estadoTipoEmpleado,$telefono1TipoEmpleado,$telefono2TipoEmpleado
-                        ,$correoTipoEmpleado,$direccionTipoEmpleado,$cuentaBancariaTipoEmpleado);
+                $Persona = new Persona($telefonoPersona, $nombrePersona, $apellido1Persona, $apellido2Persona, $tipoUsuarioPersona, $idZona,0);
 
                 $tipoEmpleadoBusiness = new tipoEmpleadoBusiness();
 
