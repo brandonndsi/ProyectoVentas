db4730c4838a94bd55498f929caf63a510c517a6
brandonndsi <brandon-ndsi@hotmail.com>
2017-08-20 23:24:09 -0600

    Actualizacion BD

--- PhpProjectVenta/backup/bdproyectoventa.sql
+++ PhpProjectVenta/backup/bdproyectoventa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-08-2017 a las 00:32:57
+-- Tiempo de generación: 21-08-2017 a las 07:22:38
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 5.6.30
 
@@ -60,6 +60,17 @@ CREATE TABLE `tbfactura` (
   `fechaFactura` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `tbfactura`
+--
+
+INSERT INTO `tbfactura` (`idFactura`, `telefonoPersona`, `fechaFactura`) VALUES
+(1, 12345436, '2017-08-01'),
+(2, 12345436, '2017-08-04'),
+(3, 88940376, '2017-08-04'),
+(4, 12345436, '2017-08-09'),
+(6, 88940376, '2017-08-11');
+
 -- --------------------------------------------------------
 
 --
@@ -67,10 +78,21 @@ CREATE TABLE `tbfactura` (
 --
 
 CREATE TABLE `tbmateriaprima` (
-  `idMateriaPrima` int(11) NOT NULL,
+  `idMateriaPrima` varchar(20) NOT NULL,
   `nombreMateriaPrima` varchar(20) NOT NULL,
   `precioMateriaPrima` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tbmateriaprima`
+--
+
+INSERT INTO `tbmateriaprima` (`idMateriaPrima`, `nombreMateriaPrima`, `precioMateriaPrima`) VALUES
+('MP1', 'Condimento', 9000),
+('MP2', 'Torta', 500),
+('MP3', 'Pan', 1200),
+('MP4', 'Tomate', 200),
+('MP5', 'Aceite', 10000);
 
 -- --------------------------------------------------------
 
@@ -88,6 +110,17 @@ CREATE TABLE `tbpersona` (
   `idEmpleado` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `tbpersona`
+--
+
+INSERT INTO `tbpersona` (`telefonoPersona`, `nombrePersona`, `apellido1Persona`, `apellido2Persona`, `tipoUsuarioPersona`, `idZona`, `idEmpleado`) VALUES
+(62091232, 'Brandon', 'Rodriguez', 'Mendez', 'Administrador', 1, 1),
+(64358765, 'Juan', 'Chavarria', 'Arroyo', 'Administrador', 3, 2),
+(73950274, 'David', 'Salas', 'Salas', 'Empleado', 4, 3),
+(82345436, 'Elisabeth', 'Rodriguez', 'Castro', 'Cliente', 2, 0),
+(88940376, 'Oscar', 'Mejias', 'Morera', 'Cliente', 2, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -99,6 +132,17 @@ CREATE TABLE `tbproducto` (
   `nombreProducto` varchar(20) NOT NULL,
   `precioProducto` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tbproducto`
+--
+
+INSERT INTO `tbproducto` (`idProducto`, `nombreProducto`, `precioProducto`) VALUES
+(1, '1Pieza', 700),
+(2, '2Piezas', 1300),
+(3, '4Piezas', 2500),
+(4, 'HamburquesaJunior', 1300),
+(5, 'Combo1', 4000);
 
 -- --------------------------------------------------------
 
@@ -118,6 +162,17 @@ CREATE TABLE `tbventa` (
   `idFactura` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `tbventa`
+--
+
+INSERT INTO `tbventa` (`idVenta`, `idEmpleado`, `cantidadProductoVenta`, `idProducto`, `idZona`, `totalVenta`, `pagaVenta`, `vueltoVenta`, `idFactura`) VALUES
+(209, 3, 1, 4, 4, 5500, 5500, 0, 4),
+(300, 2, 3, 3, 3, 6100, 7000, 900, 3),
+(404, 2, 1, 2, 1, 6500, 10000, 35000, 1),
+(507, 3, 1, 1, 2, 3500, 4000, 500, 2),
+(602, 3, 2, 1, 2, 6000, 6000, 0, 5);
+
 -- --------------------------------------------------------
 
 --
@@ -129,6 +184,16 @@ CREATE TABLE `tbzona` (
   `nombreZona` varchar(20) NOT NULL,
   `precioZona` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tbzona`
+--
+
+INSERT INTO `tbzona` (`idZona`, `nombreZona`, `precioZona`) VALUES
+(1, 'PuertoViejo', 2000),
+(2, 'Horquetas', 1000),
+(3, 'Finca6', 1000),
+(4, 'Finca11', 500);
 
 --
 -- Índices para tablas volcadas
