1ef102c26274a950726d660ad698af8f97f4020d
david <david@david-HP-Notebook>
2017-08-27 19:23:03 -0600

    listo ya estan modificadas todas las business de la carpeta business listas para textiarlas y enparejar con la base de datos ..pendiente las accion de los datos que esta en modo beta de arreglar

--- PhpProjectVenta/business/materiaprimabusiness/MateriaPrimaBusiness.php
+++ PhpProjectVenta/business/materiaprimabusiness/MateriaPrimaBusiness.php
@@ -9,37 +9,37 @@
 
 class MateriaPrimaBusiness {
 
-    private $DataEmpleado;
+    private $DataMateriaPrima;
 
     public function MateriaPrimaBusiness() {
         
         require_once '../../data/datamateriaprima/DataMateriaPrima.php';
-        $this->DataEmpleado = new DataEmpleado();
+        $this->DataMateriaPrima = new DataMateriaPrima();
     }
     
-    //se encarga de la crear el nuevo empleado a la base de datos
-    public function getTBMateriaPrimaNuevo($empleado) {
-        return $this->DataEmpleado->insertar($empleado);
+    //se encarga de la crear el nuevo materiaprima a la base de datos
+    public function getTBMateriaPrimaNuevo($materiaprima) {
+        return $this->DataMateriaPrima->insertar($materiaprima);
     }
     
-    //se encarga de actualizar los datos de la base de datos de empleado.
-    public function getTBMateriaPrimaActualizar($empleado){
-        return $this->DataEmpleado->modificar($empleado);
+    //se encarga de actualizar los datos de la base de datos de materia prima.
+    public function getTBMateriaPrimaActualizar($materiaprima){
+        return $this->DataMateriaPrima->modificar($materiaprima);
     }
     
-    //se encarga de eliminar el empleado de la tabla.
-    public function getTBMateriaPrimaEliminar($empleado) {
-        return $this->DataEmpleado->eliminar($empleado);
+    //se encarga de eliminar la materia prima de la tabla.
+    public function getTBMateriaPrimaEliminar($materiaprima) {
+        return $this->DataMateriaPrima->eliminar($materiaprima);
     }
     
-    //se encarga de realizar la consulta y retornar todos los empleado
+    //se encarga de realizar la consulta y retornar todos la materia prima
     public function getTBMateriaPrimaTodo() {
-        return $this->DataEmpleado->get_empleado();
+        return $this->DataMateriaPrima>get_materiaprima();
     }
     
-    //se encarga de buscar los datos de empleado de la base de datos
-    public function getTBMateriaPrimaBuscar($empleado) {
-        return $this->DataEmpleado->obtener($empleado);
+    //se encarga de buscar los datos de materia prima de la base de datos
+    public function getTBMateriaPrimaBuscar($materiaprima) {
+        return $this->DataMateriaPrima->obtener($materiaprima);
     }
     
 }
