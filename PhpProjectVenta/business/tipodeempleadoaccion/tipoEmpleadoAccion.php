a4522a55127ba40121b4efd800bf78f9bd8daa5d
david <david@david-HP-Notebook>
2017-08-28 11:47:29 -0600

    final del aporte de david hottttttttttttttttttttt

--- PhpProjectVenta/business/tipodeempleadoaccion/tipoEmpleadoAccion.php
+++ PhpProjectVenta/business/tipodeempleadoaccion/tipoEmpleadoAccion.php
@@ -3,7 +3,6 @@
  * Descripcion valida que los datos y la accion a realizar sean correcta sino redirecciona
  * a paguinas de error y si los datos y la accion es correcta imboca a la businerss para
  * que se haga cargo de los datos y el metodo a efectuar.
- *
  * @author David Salas.
  */
 $accion = $_POST['accion'];//busca la accion a realizar 
